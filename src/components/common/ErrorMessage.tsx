99a0e277a0810e837d30644fb62d6ea945667c72
Sathish Srinivasan Radhakrishnan <srsathishh@gmail.com>
2025-01-26 12:04:55 +0530

    update ErrorMessage file to tsx, fix imports and required type updates

--- src/components/common/ErrorMessage.tsx
+++ src/components/common/ErrorMessage.tsx
@@ -1,18 +1,25 @@
-import React from "react";
-import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import "./ErrorMessage.css";
 
-const ErrorMessage = ({ isError, errorMessage, onClick, ...props }) => {
+interface ErrorMessageProps {
+  isError: boolean;
+  errorMessage: string;
+  onClick: () => void;
+}
+
+const ErrorMessage: React.FC<ErrorMessageProps> = ({ 
+  isError, 
+  errorMessage, 
+  onClick, 
+  ...props 
+}) => {
   return (
     <div
       className={`error-message-container${isError ? " error" : ""}`}
       {...props}
     >
       {isError && (
-        // This component is not structured how it should,
-        // But this is done to keep backwards compatibility
         <h3 data-test="error">
           <button
             className="error-button"
@@ -26,21 +33,6 @@ const ErrorMessage = ({ isError, errorMessage, onClick, ...props }) => {
       )}
     </div>
   );
-};
-
-ErrorMessage.propTypes = {
-  /**
-   * If this is an isError field yes or no
-   */
-  isError: PropTypes.bool.isRequired,
-  /**
-   * The value of the input
-   */
-  errorMessage: PropTypes.string.isRequired,
-  /**
-   * The on change handler
-   */
-  onClick: PropTypes.func.isRequired,
 };
 
 export default ErrorMessage;
