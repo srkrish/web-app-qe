8ccc1830ed87423b3f966516148ddc75d43c1cd2
Sathish Srinivasan Radhakrishnan <srsathishh@gmail.com>
2025-04-14 00:44:46 +1000

    setup the repo to run storybook tests

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,7 +4,10 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/preset-create-react-app',
-    'storybook-addon-react-router-v6'
+    'storybook-addon-react-router-v6',
+    "@storybook/addon-actions",
+    "@storybook/addon-controls",
+    '@storybook/addon-interactions'
   ],
   framework: {
     name: '@storybook/react-webpack5',
